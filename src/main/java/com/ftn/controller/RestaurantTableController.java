78fc6052766d5473060f06aa77d65b34480cffc4
Aleksandar Radosevic <acafacafunnyface@gmail.com>
2017-02-26 00:54:31 +0100

    Added creating Restaurant Table in areas, while its front end is still in process of making

---
+++ src/main/java/com/ftn/controller/RestaurantTableController.java
@@ -0,0 +1,7 @@
+package com.ftn.controller;
+
+/**
+ * Created by Alek on 2/26/2017.
+ */
+public class RestaurantTableController {
+}
---
+++ src/main/java/com/ftn/repository/RestaurantTableDao.java
@@ -0,0 +1,7 @@
+package com.ftn.repository;
+
+/**
+ * Created by Alek on 2/26/2017.
+ */
+public interface RestaurantTableDao {
+}
