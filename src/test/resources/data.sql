50a6f8f28624384ccdc6116cd1b3422851dbbc32
Aleksandar Radosevic <acafacafunnyface@gmail.com>
2017-03-01 01:54:54 +0100

    Deploying detabase sets

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -6,12 +6,74 @@ INSERT INTO user (active, created, updated, confirmationcode, email, enabled, fi
 INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-02-27 21:39:06', null, '05bb039b-a176-4e36-a130-5eef3f2bd81a', 'akahriman@execom.eu', true, 'Aleksandar', 'Kahriman', '$2a$10$cKTGQ6D82GJjdks6pkGVQ.gtOPx1BqTbWspAGkDwPxdNzmIWtwLZK', 'GUEST');
 INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-02-27 23:38:22', null, '373e7bbf-7d0d-4937-9348-d6864fcb2a99', 'alek@gmail.com', true, 'Alek', 'Radosevic', '$2a$10$u3o2TuM8rjkOfxMfJFd5y.5SesqpGfx0iP9nIoSZQJjelJIgU21km', 'GUEST');
 INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-02-27 23:38:48', null, '7f2b8e66-c060-48b8-bf73-768a6cd31515', 'bojan@gmail.com', true, 'Boki', 'Boki', '$2a$10$vBEniicn.Zu7i0vhylhBdeV1flKVwvh2gpgPYS73q6677aQu.tijC', 'GUEST');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-02-28 20:20:28', null, null, 'weiwei@gmail.com', true, 'wei', 'wei', '$2a$10$v/v.KBoeFStWz/M5X3JC1eAx3FGeDi7pTQ2YFcXYsbeI1ATPaY4d.', 'WAITER');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:11:07', null, null, 'sys1@gmail.com', true, 'System', 'Manager1', '$2a$10$0ZSVkIKEl2vyRMxR2KAA7OeBmwwpjn5PqFVcWPv/nexYAQyTwnMIS', 'SYSTEM_MANAGER');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:11:24', null, null, 'sys2@gmail.com', true, 'System', 'Manager2', '$2a$10$v8wV44KxtKq6RUx/bN2dZuAaaoCYMu4UNhD13tShsoMX951vzHXMK', 'SYSTEM_MANAGER');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:12:11', null, null, 'resmen1@gmail.com', true, 'Restaurant', 'Manager1', '$2a$10$xCsffq1/bKxV6NXyXXMFqerIHCtTS5UamBY3rYbviG6s7Jikggu06', 'MANAGER');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:20:03', null, null, 'resmen2@gmail.com', true, 'Restaurant', 'Manager2', '$2a$10$w7KKXXaTWkmEH4giulMpRedr8/7GhYNxl6bKZNjaiYrKQLJn8WFii', 'MANAGER');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:20:28', null, null, 'resmen3@gmail.com', true, 'Restaurant', 'Manager', '$2a$10$rAKIosHos9egZ65ZAyxNOuNOs6didIYavU3M/NTChHujN/D/jm4sO', 'MANAGER');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:21:00', null, null, 'resmen4@gmail.com', true, 'Restaurant', 'Manager4', '$2a$10$Yw0QJ0TJ37Vt0HUM3Rclj.jKzErieut9WdNFdS/KzA/IGZmq4sF8q', 'MANAGER');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:23:06', null, null, 'waiter@gmail.com', true, 'Waiter', 'Waiter1', '$2a$10$tF.KXxgwGL9UBGuDYNQNm.cH4cYxLmXhNTni2C8AjjlTPJi5Ork5S', 'WAITER');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:24:11', null, null, 'seller1@gmail.com', true, 'Seller', 'Seller1', '$2a$10$ZyGEtQ19c7HQgzJVlxrd6eYPflRChzVZXA97atlp6CNPNEXgdONDe', 'SELLER');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:24:37', null, null, 'seller2@gmail.com', true, 'Seller', 'Seller2', '$2a$10$zgis5DpHJgt/MHbhqcXzLeJxfH1URA.LoKz9hp5q/M9QRuXGUk5py', 'SELLER');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:24:55', null, null, 'seller3@gmail.com', true, 'Seller', 'Seller3', '$2a$10$ZFrs7xItwwdYNKkYUfW.I.TcLh0ameENhLzH0Z8QIZQr8L3Yh9Hyu', 'SELLER');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:26:26', null, null, 'waiter2@gmail.com', true, 'Waiter', 'Waiter2', '$2a$10$dAfoAQP4vgmn.Xk3nXySnOcS2ufwbGcCsbCdVWdBN0GKeQu4HeMby', 'WAITER');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:26:49', null, null, 'waiter3@gmail.com', true, 'Waiter', 'Waiter3', '$2a$10$RrBaIHbYxIiIWKbkRkuSw.KQBUrnM/AL321svvT/AWTeL8o6oRJfa', 'WAITER');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:27:16', null, null, 'bartender@gmail.com', true, 'Bartender', 'Bartender', '$2a$10$/gZtgDNjVW1PWbKOEFa0COdknWQEv9ncC17XFTNFakM4EEr/Zzxcu', 'BARTENDER');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:27:32', null, null, 'bartender1@gmail.com', true, 'Bartender', 'Bartender', '$2a$10$URFJk/pORFZ0ktsuyjZTjOepiLd2xJ5vIdFXvy51s23wVKStn8qhu', 'BARTENDER');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:27:50', null, null, 'bartender2@gmail.com', true, 'Bartender', 'Bartender2', '$2a$10$nmRnK7YoBstVLD0HYQBA3OsfCNljo0babbN7uT6fCjqMalxzR7kk6', 'BARTENDER');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:28:28', null, null, 'chef@gmail.com', true, 'Chef', 'Chef1', '$2a$10$s2ylplHmXoUvSdg4A1WO9.q5BsOZUwIK2KnvLMqTEh6MwA3G6.FNm', 'CHEF');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:28:51', null, null, 'chef1@gmail.com', true, 'Chef', 'Chef1', '$2a$10$TDBT5CrzwM7dPriIBhZ.fe85fspbxOm5i1KZdoyRXr1zwrUeDvCEG', 'CHEF');
+INSERT INTO user (active, created, updated, confirmationcode, email, enabled, firstname, lastname, password, role) VALUES (true, '2017-03-01 01:29:15', null, null, 'chef2@gmail.com', true, 'Chef', 'Chef2', '$2a$10$uLqFiCDpGb8yvy..AcTAF.N4eAhHI2EHRKc8e0EY7vbh.iCql2j0q', 'CHEF');
 
 INSERT INTO guest (id) VALUES (1);
 INSERT INTO guest (id) VALUES (5);
 INSERT INTO guest (id) VALUES (6);
 INSERT INTO guest (id) VALUES (7);
 INSERT INTO guest (id) VALUES (8);
+
+INSERT INTO restaurant_type (active, created, updated, description, name) VALUES (true, '2017-02-28 00:14:43', null, 'fastfood', 'Fastfood');
+INSERT INTO restaurant_type (active, created, updated, description, name) VALUES (true, '2017-03-01 01:12:25', null, 'vegetarian', 'Vegetarian');
+INSERT INTO restaurant_type (active, created, updated, description, name) VALUES (true, '2017-03-01 01:12:35', null, 'chinese', 'Chinese');
+
+INSERT INTO systemmanager (id) VALUES (2);
+INSERT INTO systemmanager (id) VALUES (10);
+INSERT INTO systemmanager (id) VALUES (11);
+
+INSERT INTO restaurant (active, created, updated, description, name, restauranttype_id, city, country, street, street_number) VALUES (true, '2017-02-28 00:14:54', null, 'KFC', 'KFC', 1, null, null, null, 0);
+INSERT INTO restaurant (active, created, updated, description, name, restauranttype_id, city, country, street, street_number) VALUES (true, '2017-03-01 01:14:56', null, 'Mek', 'Mc Donald''s', 1, null, null, null, 0);
+INSERT INTO restaurant (active, created, updated, description, name, restauranttype_id, city, country, street, street_number) VALUES (true, '2017-03-01 01:15:14', null, 'Chinese fast food restaurant', 'Two Sticks', 3, null, null, null, 0);
+INSERT INTO restaurant (active, created, updated, description, name, restauranttype_id, city, country, street, street_number) VALUES (true, '2017-03-01 01:17:29', null, 'Famous Djokovic restaurant', 'Djokovic', 2, null, null, null, 0);
+INSERT INTO restaurant (active, created, updated, description, name, restauranttype_id, city, country, street, street_number) VALUES (true, '2017-03-01 01:18:05', null, 'Two dogs', 'Two Dogs', 3, null, null, null, 0);
+INSERT INTO restaurant (active, created, updated, description, name, restauranttype_id, city, country, street, street_number) VALUES (true, '2017-03-01 01:18:18', null, 'Best in the world', 'Vegan Palace', 2, null, null, null, 0);
+
+INSERT INTO manager (id, restaurant) VALUES (3, 1);
+INSERT INTO manager (id, restaurant) VALUES (12, 1);
+INSERT INTO manager (id, restaurant) VALUES (14, 3);
+INSERT INTO manager (id, restaurant) VALUES (15, 4);
+INSERT INTO manager (id, restaurant) VALUES (13, 6);
+
+INSERT INTO seller (id) VALUES (4);
+INSERT INTO seller (id) VALUES (19);
+INSERT INTO seller (id) VALUES (17);
+INSERT INTO seller (id) VALUES (18);
+
+INSERT INTO area (active, created, updated, name, restaurant) VALUES (true, '2017-02-28 20:20:13', null, 'Smoking', 1);
+INSERT INTO area (active, created, updated, name, restaurant) VALUES (true, '2017-03-01 01:23:15', null, 'Garden', 1);
+INSERT INTO area (active, created, updated, name, restaurant) VALUES (true, '2017-03-01 01:23:25', null, 'Non-Smking', 1);
+
+INSERT INTO bartender (birthdate, dresssize, footwearsize, id, restaurant) VALUES (null, 10, 10, 24, 1);
+INSERT INTO bartender (birthdate, dresssize, footwearsize, id, restaurant) VALUES (null, 1, 1, 22, 1);
+INSERT INTO bartender (birthdate, dresssize, footwearsize, id, restaurant) VALUES (null, 2, 2, 23, 1);
+
+INSERT INTO chef (birthdate, dresssize, footwearsize, profile, id, restaurant) VALUES (null, 10, 10, 'Soups', 27, 1);
+INSERT INTO chef (birthdate, dresssize, footwearsize, profile, id, restaurant) VALUES (null, 1, 1, 'Grilled meals', 25, 1);
+INSERT INTO chef (birthdate, dresssize, footwearsize, profile, id, restaurant) VALUES (null, 2, 2, 'Desserts', 26, 1);
+
+INSERT INTO waiter (birthdate, dresssize, footwearsize, id, area, restaurant) VALUES (null, 3, 3, 9, 3, 1);
+INSERT INTO waiter (birthdate, dresssize, footwearsize, id, area, restaurant) VALUES (null, 3, 7, 16, 1, 1);
+INSERT INTO waiter (birthdate, dresssize, footwearsize, id, area, restaurant) VALUES (null, 2, 2, 21, 2, 1);
+INSERT INTO waiter (birthdate, dresssize, footwearsize, id, area, restaurant) VALUES (null, 3, 3, 20, 3, 1);
 
 INSERT INTO friendship (active, created, updated, status, originator_id, recipient_id) VALUES (true, '2017-02-27 23:41:27', '2017-02-27 23:41:47', 'ACCEPTED', 5, 6);
 INSERT INTO friendship (active, created, updated, status, originator_id, recipient_id) VALUES (true, '2017-02-27 23:41:32', '2017-02-27 23:42:49', 'ACCEPTED', 5, 8);
