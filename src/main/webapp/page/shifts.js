f2c86276abfa6bcb0e7361e12ca565e01f4f250a
Aleksandar Radosevic <acafacafunnyface@gmail.com>
2017-03-01 05:57:27 +0100

    Few changes to shifts and items

--- src/main/webapp/page/shifts.js
+++ src/main/webapp/page/shifts.js
@@ -2,8 +2,8 @@ app.controller('ShiftsController', function ($scope, $http, $state, $filter, $lo
 
     $scope.page.current = 17;
 
-    $scope.selectUntil = [];
-    $scope.selectFrom = [];
+    $scope.dayAh = 0;
+    $scope.dayBh = 0;
     $scope.shifts = [];
     $scope.parsedShifts = [];
     $scope.employeeShifts = [];
@@ -34,7 +34,7 @@ app.controller('ShiftsController', function ($scope, $http, $state, $filter, $lo
 
     loadShifts();
 
-    $scope.addShift = function() {
+    $scope.addShift = function () {
         $mdDialog.show({
             parent: angular.element(document.body),
             templateUrl: 'dialog/createShift.html',
@@ -44,7 +44,7 @@ app.controller('ShiftsController', function ($scope, $http, $state, $filter, $lo
         });
     };
 
-    $scope.addEmployeeShift = function() {
+    $scope.addEmployeeShift = function () {
         $mdDialog.show({
             parent: angular.element(document.body),
             templateUrl: 'dialog/createEmployeeShift.html',
@@ -54,35 +54,38 @@ app.controller('ShiftsController', function ($scope, $http, $state, $filter, $lo
         });
     };
 
-    $scope.searchShifts = function() {
+    $scope.searchShifts = function () {
         employeeShiftsService.list(function (response) {
             $scope.employeeShifts = [];
             $scope.parsedEmployeeShifts = [];
             $scope.employeeShifts = response.data;
             $scope.employeeShifts.forEach(function (shift) {
-                shift.date = moment(shift.date).format("YYYY-MM-DD");
+                shift.date = moment(shift.date).format("DD-MM-YYYY");
                 $scope.parsedEmployeeShifts.push(shift);
             });
-            $scope.selectUntil = moment($scope.selectUntil).format("YYYY-MM-DD");
-            $scope.selectFrom = moment($scope.selectFrom).format("YYYY-MM-DD");
+            var weekDay = new Date();
+            weekDay.setDate(7);
+            weekDay = moment(weekDay).format("DD-MM-YYYY");
+            var weekBehind = new Date();
+            weekBehind.setDate(-7);
+            $scope.employeeShifts = [];
+            weekBehind = moment(weekBehind).format("DD-MM-YYYY");
             $scope.parsedEmployeeShifts.forEach(function (parsedShift) {
-                $scope.employeeShifts = [];
-                if (parsedShift.date < $scope.selectUntil){
+                if (parsedShift.date < weekDay) {
                     $scope.employeeShifts.push(parsedShift);
                 }
             });
         });
-        $scope.selectUntil = [];
-        $scope.selectFrom = [];
     };
+
 
     // Calendar demo
     $scope.selectedDate = null;
     $scope.firstDayOfWeek = 1;
-    $scope.setDirection = function(direction) {
+    $scope.setDirection = function (direction) {
         $scope.direction = direction;
     };
-    $scope.dayClick = function(date) {
+    $scope.dayClick = function (date) {
         $mdDialog.show({
             parent: angular.element(document.body),
             templateUrl: 'dialog/createEmployeeShift.html',
@@ -98,64 +101,64 @@ app.controller('ShiftsController', function ($scope, $http, $state, $filter, $lo
 
     var html = null;
 
-    $scope.setDayContent = function(date) {
+    $scope.setDayContent = function (date) {
         // You would inject any HTML you wanted for
         // that particular date here.
         employeeShiftsService.list(function (response) {
             /*$scope.employeeShifts = [];
-            $scope.employeeShifts = response.data;
-            for (var i in $scope.employeeShifts){
-                $scope.dateToCompare = moment($scope.employeeShifts[i].date).format("YYYY-MM-DD");
-                $scope.employeeShifts[i].date = moment($scope.employeeShifts[i].date).format("YYYY-MM-DD");
-                date = moment(date).format("YYYY-MM-DD");
-                if ($scope.dateToCompare == date){
-                    $scope.found = $scope.employeeShifts[i].date;
-                    //$scope.msg = "Date was found " + $filter("date")(date, "MMM d, y h:mm:ss a Z");
-                    $scope.msg += ii + "_FOUND";
-                    ii++;
-                    html = '<div id="progressBarOuter"><div id="bytesLoaded"></div><div id="progressBar"></div></div><div id="currentTime">Found</div>';
-                    break;
-                }
-                else {
-                    html = null;
-                }
-            }*/
+             $scope.employeeShifts = response.data;
+             for (var i in $scope.employeeShifts){
+             $scope.dateToCompare = moment($scope.employeeShifts[i].date).format("YYYY-MM-DD");
+             $scope.employeeShifts[i].date = moment($scope.employeeShifts[i].date).format("YYYY-MM-DD");
+             date = moment(date).format("YYYY-MM-DD");
+             if ($scope.dateToCompare == date){
+             $scope.found = $scope.employeeShifts[i].date;
+             //$scope.msg = "Date was found " + $filter("date")(date, "MMM d, y h:mm:ss a Z");
+             $scope.msg += ii + "_FOUND";
+             ii++;
+             html = '<div id="progressBarOuter"><div id="bytesLoaded"></div><div id="progressBar"></div></div><div id="currentTime">Found</div>';
+             break;
+             }
+             else {
+             html = null;
+             }
+             }*/
             /*$scope.employeeShifts.forEach(function (employeeShift) {
-                employeeShift.date = moment(employeeShift.date).format("YYYY-MM-DD");
-                date = moment(date).format("YYYY-MM-DD");
-                if (employeeShift.date == date){
-                    $scope.found = employeeShift.date;
-                    //$scope.msg = "Date was found " + $filter("date")(date, "MMM d, y h:mm:ss a Z");
-                    html = '<div id="progressBarOuter"><div id="bytesLoaded"></div><div id="progressBar"></div></div><div id="currentTime">Found</div>';
-                    $scope.msg += i + "_FOUND";
-                    i++;
-                    return html;
-                }
-            });
-            html = '<div id="progressBarOuter"><div id="bytesLoaded"></div><div id="progressBar"></div></div><div id="currentTime">Found</div>';*/
+             employeeShift.date = moment(employeeShift.date).format("YYYY-MM-DD");
+             date = moment(date).format("YYYY-MM-DD");
+             if (employeeShift.date == date){
+             $scope.found = employeeShift.date;
+             //$scope.msg = "Date was found " + $filter("date")(date, "MMM d, y h:mm:ss a Z");
+             html = '<div id="progressBarOuter"><div id="bytesLoaded"></div><div id="progressBar"></div></div><div id="currentTime">Found</div>';
+             $scope.msg += i + "_FOUND";
+             i++;
+             return html;
+             }
+             });
+             html = '<div id="progressBarOuter"><div id="bytesLoaded"></div><div id="progressBar"></div></div><div id="currentTime">Found</div>';*/
         });
         /*var datez = [];
-        var datee1 = new Date();
-        var datee2 = new Date();
-        date = moment(date).format("YYYY-MM-DD");
-        datee1 = moment(datee1).format("YYYY-MM-DD");
-        datee2 = moment(datee2).format("YYYY-MM-DD");
-        datez.push(datee1);
-        datez.push(datee2);
-        for (var d in datez){
-            if (datez[d] == date){
-                return html = '<div id="progressBarOuter"><div id="bytesLoaded"></div><div id="progressBar"></div></div><div id="currentTime">Found</div>';
-            }
-            else {
-                html = null;
-            }
-        }*/
+         var datee1 = new Date();
+         var datee2 = new Date();
+         date = moment(date).format("YYYY-MM-DD");
+         datee1 = moment(datee1).format("YYYY-MM-DD");
+         datee2 = moment(datee2).format("YYYY-MM-DD");
+         datez.push(datee1);
+         datez.push(datee2);
+         for (var d in datez){
+         if (datez[d] == date){
+         return html = '<div id="progressBarOuter"><div id="bytesLoaded"></div><div id="progressBar"></div></div><div id="currentTime">Found</div>';
+         }
+         else {
+         html = null;
+         }
+         }*/
         /*if (date == datee){
-            return html = '<div id="progressBarOuter"><div id="bytesLoaded"></div><div id="progressBar"></div></div><div id="currentTime">Found</div>';
-        }
-        else {
-            html = null;
-        }*/
+         return html = '<div id="progressBarOuter"><div id="bytesLoaded"></div><div id="progressBar"></div></div><div id="currentTime">Found</div>';
+         }
+         else {
+         html = null;
+         }*/
         return html;
     };
 });
